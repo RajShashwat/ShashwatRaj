b1a907b4294098134f50a2b48a948f873644726e
Shashwat Raj <shashwat.r2018bcs@srisriuniversity.edu.in>
2020-05-24 01:35:51 +0530

    Updated the look

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -3,3 +3,11 @@ $('.datepicker').each(function(){
 		field: this
 	});
 });
+function myFunction(imgs) {
+  var expandImg = document.getElementById("expandedImg");
+  var imgText = document.getElementById("imgtext");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
+	expandImg.parentElement.style.align = "center";
+}
